be73cb650fabe67acc62fd1a208b01f0c66f2225
John Aguirre <johnzcode@gmail.com>
2024-12-21 15:21:10 -0500

    agrego header al template principal para que quede en toda la app y agrego componente para ir agregando los productos al list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./domains/shared/components/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: '<router-outlet />'
+  imports: [RouterOutlet, HeaderComponent],
+  template: `<app-header /> <router-outlet />`
 })
 export class AppComponent {
   title = 'store';
--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ProductComponent } from '../../components/product/product.component';
+import { Product } from '../../../shared/models/product.model';
+import { HeaderComponent } from "../../../shared/components/header/header.component";
 
 @Component({
   selector: 'app-list',
-  imports: [ProductComponent],
+  imports: [ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  products = signal<Product[]>([]);
 
 }
