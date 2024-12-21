8d9d06a9580d456c7db00a8e1ddf71ee355716d8
John Aguirre <johnzcode@gmail.com>
2024-12-21 17:03:45 -0500

    quito header component del list ya que esta en el component principal

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { ProductComponent } from '../../components/product/product.component';
 import { Product } from '../../../shared/models/product.model';
-import { HeaderComponent } from "../../../shared/components/header/header.component";
 
 @Component({
   selector: 'app-list',
-  imports: [ProductComponent, HeaderComponent],
+  imports: [ProductComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
