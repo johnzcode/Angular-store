c49f47ab86a36db678ecf6043990d6df8c16016f
John Aguirre <johnzcode@gmail.com>
2024-12-22 17:30:08 -0500

    agrego header al componente principal para no estar importando el header en cada componente

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,13 +1,12 @@
 import { Component, inject, signal } from '@angular/core';
 import { ProductComponent } from '@products/components/product/product.component';
 import { Product } from '@shared/models/product.model';
-import { HeaderComponent } from '@shared/components/header/header.component';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
 
 @Component({
   selector: 'app-list',
-  imports: [ProductComponent, HeaderComponent],
+  imports: [ProductComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
