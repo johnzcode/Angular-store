b229fcd5206e7cd1efe3835db55c1087b2930c28
John Aguirre <johnzcode@gmail.com>
2024-12-21 20:40:02 -0500

    hago uso del servicio para mostrar los productos en el list component

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -3,6 +3,7 @@ import { ProductComponent } from '../../components/product/product.component';
 import { Product } from '../../../shared/models/product.model';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { CartService } from '../../../shared/services/cart.service';
+import { ProductService } from '../../../shared/services/product.service';
 
 @Component({
   selector: 'app-list',
@@ -14,7 +15,18 @@ export class ListComponent {
 
   products = signal<Product[]>([]);
   private cartService = inject(CartService);
+  private productService = inject(ProductService);
 
+  ngOnInit(){
+    this.productService.getProducts().subscribe({
+      next: (products) => {
+        this.products.set(products);
+      },
+      error: () => {
+        
+      }
+    })
+  }
 
   addToCart(product: Product){
     this.cartService.addToCart(product);
