79b89385979da6b4d1febddf86c2ab4e2af006c9
John Aguirre <johnzcode@gmail.com>
2024-12-22 17:17:40 -0500

    agrego routerlink para evitar sobre recargas a la pagina cuando vamos a otro enlace de la misma app

--- src/app/domains/info/pages/not-found/not-found.component.ts
+++ src/app/domains/info/pages/not-found/not-found.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [RouterLinkWithHref],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
 })
