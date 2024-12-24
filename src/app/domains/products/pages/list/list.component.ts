ff2bba093eb0268e33fd86995f34073b7b2653b1
John Aguirre <johnzcode@gmail.com>
2024-12-23 22:33:27 -0500

    agrego url params para filtar por id de categoria

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,14 +1,15 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, Input, signal, SimpleChanges } from '@angular/core';
 import { ProductComponent } from '@products/components/product/product.component';
 import { Category } from '@shared/models/category.model';
 import { Product } from '@shared/models/product.model';
 import { CartService } from '@shared/services/cart.service';
 import { CategoryService } from '@shared/services/category.service';
 import { ProductService } from '@shared/services/product.service';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-list',
-  imports: [ProductComponent],
+  imports: [ProductComponent, RouterLinkWithHref],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
@@ -19,10 +20,14 @@ export class ListComponent {
   private cartService = inject(CartService);
   private productService = inject(ProductService);
   private categoryService = inject(CategoryService);
+  @Input() category_id?: string;
 
   ngOnInit(){
-    this.getProducts();
     this.getCategories();
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+    this.getProducts();
   }
 
   addToCart(product: Product){
@@ -30,7 +35,7 @@ export class ListComponent {
   }
 
   private getProducts(){
-    this.productService.getProducts().subscribe({
+    this.productService.getProducts(this.category_id).subscribe({
         next: (products) => {
             this.products.set(products);
         },
