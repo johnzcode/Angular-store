945246a6990275731fabcb01e8d4b055888ff357
John Aguirre <johnzcode@gmail.com>
2024-12-23 21:41:39 -0500

    agrego api para obtener un producto por id

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, signal } from '@angular/core';
+import { Product } from '@shared/models/product.model';
+import { ProductService } from '@shared/services/product.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-detail',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
 export class ProductDetailComponent {
+
+    @Input() id?: string;
+    product = signal<Product | null>(null);
+    private productService = inject(ProductService);
+
+    ngOnInit(){
+        if (this.id){
+            this.productService.getOne(this.id).subscribe({
+                next: (product) => {
+                    this.product.set(product);
+                }
+            });
+        }
+    }
 
 }
