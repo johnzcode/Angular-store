04f116a782b5ed137f11591d91bbb478ab5fabcc
John Aguirre <johnzcode@gmail.com>
2024-12-23 22:15:04 -0500

    agrego lista de categorias y separo por metodos en el ngOnInit

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,7 +1,9 @@
 import { Component, inject, signal } from '@angular/core';
 import { ProductComponent } from '@products/components/product/product.component';
+import { Category } from '@shared/models/category.model';
 import { Product } from '@shared/models/product.model';
 import { CartService } from '@shared/services/cart.service';
+import { CategoryService } from '@shared/services/category.service';
 import { ProductService } from '@shared/services/product.service';
 
 @Component({
@@ -13,21 +15,40 @@ import { ProductService } from '@shared/services/product.service';
 export class ListComponent {
 
   products = signal<Product[]>([]);
+  categories = signal<Category[]>([]);
   private cartService = inject(CartService);
   private productService = inject(ProductService);
+  private categoryService = inject(CategoryService);
 
   ngOnInit(){
-    this.productService.getProducts().subscribe({
-      next: (products) => {
-        this.products.set(products);
-      },
-      error: () => {
-        
-      }
-    })
+    this.getProducts();
+    this.getCategories();
   }
 
   addToCart(product: Product){
     this.cartService.addToCart(product);
   }
+
+  private getProducts(){
+    this.productService.getProducts().subscribe({
+        next: (products) => {
+            this.products.set(products);
+        },
+        error: () => {
+            
+        }
+    })
+  }
+
+  private getCategories(){
+    this.categoryService.getAll().subscribe({
+        next: (data) => {
+            this.categories.set(data);
+        },
+        error: () => {
+            
+        }
+    })
+  }
+
 }
