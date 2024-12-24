227a3a17c63aca7af21ba1c19b3cb8208894cdff
John Aguirre <johnzcode@gmail.com>
2024-12-23 21:46:17 -0500

    agrego detalle obtenido por el api y creo interface de categoria para mostrar el detalle de cada categoria

--- src/app/domains/products/pages/product-detail/product-detail.component.ts
+++ src/app/domains/products/pages/product-detail/product-detail.component.ts
@@ -13,6 +13,7 @@ export class ProductDetailComponent {
 
     @Input() id?: string;
     product = signal<Product | null>(null);
+    cover = signal('');
     private productService = inject(ProductService);
 
     ngOnInit(){
@@ -20,9 +21,16 @@ export class ProductDetailComponent {
             this.productService.getOne(this.id).subscribe({
                 next: (product) => {
                     this.product.set(product);
+                    if (product.images.length > 0){
+                        this.cover.set(product.images[0]);
+                    }
                 }
             });
         }
+    }
+
+    changeCover(newImg: string){
+        this.cover.set(newImg);
     }
 
 }
